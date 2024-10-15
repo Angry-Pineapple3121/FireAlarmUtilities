7b871e880ab82bc62698675c0d8515572d428e84
Angry <mitjamdou@gmail.com>
2024-10-15 16:04:56 -0400

    version 0.8.2 - add support for loganetics devices to date code decoder module

--- cogs/datecode.py
+++ cogs/datecode.py
@@ -23,20 +23,21 @@ def convert_date_code(date_code):
 
     return formatted_date
 
-class SimplexDCD(commands.Cog):
-    print('★ Loaded cog: Simplex Date Code Decoder')
+class DateCode(commands.Cog):
+    print('★ Loaded cog: Date Code Decoder')
     def __init__(self, bot):
         self.bot = bot
 
     @discord.slash_command()
-    async def simplexdcd(
+    async def dcd(
         self,
         ctx,
-        datecode: Option(str, 'The date code of the device you want to view. Should be in a format similar to "22 107 V".', required=True),
+        brand: Option(str, 'The brand of the device you want to view.', choices=['Simplex', 'Loganetics'], required=True),
+        datecode: Option(str, 'The date code of the device. Format: "22 107 V" (Simplex) or "24241A" (Loganetics).', required=True),
     ):
         """Checks when a Simplex device was manufactured based on its date code."""
         requestTime = datetime.now()
-        print(f'[{requestTime}] [Simplex DCD] Requested by {ctx.author} (ID: {ctx.author.id}) with date code {datecode}')
+        print(f'[{requestTime}] [DCD] Requested by {ctx.author} (ID: {ctx.author.id}) with date code {datecode}')
 
         with open('blacklist', 'r') as file:
             GLOBAL_BLACKLIST = [int(line.strip()) for line in file if line.strip()]
@@ -45,41 +46,83 @@ class SimplexDCD(commands.Cog):
             await ctx.respond('<:X_:1152069831638650890> Your account is currently **blacklisted** from all features in Fire Alarm Utilities.')
             print(f'[{datetime.now()}] [Blacklisted] {ctx.author} (ID: {ctx.author.id}) attempted to use a command they are blacklisted from using.')
         else:
-            try:
-                embed = discord.Embed(
-                    title=f"<:slash:1150933397179486339> Simplex Date Code Decoder",
-                    description=f'Finished decoding date code **{datecode}**.',
-                    color=discord.Colour.fuchsia(),
-                )
-    
-                now = datetime.now()
-                rtime = now.strftime("%B %d, %Y, %H:%M")
-                embed.set_footer(text=f"Requested by {ctx.author.display_name} » {rtime} | {BOT_VERSION}")
-    
-                # regex for our date code
-                code_regex = '[0-9]{2} [0-9]{3}'
-
-                # check to see if we match the date code and throw error if we don't
+            # Simplex date code decoder
+            if brand == 'Simplex':
                 try:
-                    if not re.match(code_regex, datecode).group():
+                    embed = discord.Embed(
+                        title=f"<:slash:1150933397179486339> Simplex Date Code Decoder",
+                        description=f'Finished decoding date code **{datecode}**.',
+                        color=discord.Colour.fuchsia(),
+                    )
+
+                    now = datetime.now()
+                    rtime = now.strftime("%B %d, %Y, %H:%M")
+                    embed.set_footer(text=f"Requested by {ctx.author.display_name} » {rtime} | {BOT_VERSION}")
+
+                    # regex for our date code
+                    code_regex = '[0-9]{2} [0-9]{3}'
+
+                    # check to see if we match the date code and throw error if we don't
+                    try:
+                        if not re.match(code_regex, datecode).group():
+                            return
+                    except:
+                        await ctx.respond('<:warn:1105998033335898162> The date code you entered does not follow the standard (moden) date code format.', ephemeral=True)
                         return
-                except:
-                    await ctx.respond('<:warn:1105998033335898162> The date code you entered does not follow the standard (moden) date code format.', ephemeral=True)
-                    return
-                # if no error match again and do the stuff
-                else:
-                    date_code = re.match(code_regex, datecode).group()
-                    
-                    readable_date = convert_date_code(date_code)
+                    # if no error match again and do the stuff
+                    else:
+                        date_code = re.match(code_regex, datecode).group()
 
-                    embed.add_field(name=f":gear: Manufacturing Date", value=f'**```{readable_date}```**', inline=True)
-                    await ctx.respond(embed=embed)
-                
+                        readable_date = convert_date_code(date_code)
 
-            except Exception as e:
-                await ctx.respond('<:warn:1105998033335898162> An error occurred when processing your command. Please contact `Angry_Pineapple#6926` with what you were attempting to do, along with the date and time.', ephemeral=True)
-                print(f'[{requestTime}] [*** BOT ERROR ***] {e}')
-                pass
+                        embed.add_field(name=f":gear: Manufacturing Date", value=f'**```{readable_date}```**', inline=True)
+                        await ctx.respond(embed=embed)
+
+                except Exception as e:
+                    await ctx.respond('<:warn:1105998033335898162> An error occurred when processing your command. Please contact `Angry_Pineapple#6926` with what you were attempting to do, along with the date and time.', ephemeral=True)
+                    print(f'[{requestTime}] [*** BOT ERROR ***] {e}')
+                    pass
+
+            # Loganetics date code decoder
+            else:
+                try:
+                    embed = discord.Embed(
+                        title=f"<:slash:1150933397179486339> Loganetics Date Code Decoder",
+                        description=f'Finished decoding date code **{datecode}**.',
+                        color=discord.Colour.random()
+                    )
+
+                    now = datetime.now()
+                    rtime = now.strftime("%B %d, %Y, %H:%M")
+                    embed.set_footer(text=f"Requested by {ctx.author.display_name} » {rtime} | {BOT_VERSION}")
+
+                    # regex for our date code
+                    code_regex = '[0-9]{2}[0-9]{3}([A-Z a-z]{1})?'
+
+                    # check to see if we match the date code and throw error if we don't
+                    try:
+                        if not re.match(code_regex, datecode).group():
+                            return
+                    except:
+                        await ctx.respond('<:warn:1105998033335898162> The date code you entered does not follow the Loganetics date code format.', ephemeral=True)
+                        return
+                    # if no error match again and do the stuff
+                    else:
+                        date_code = re.match(code_regex, datecode).group()
+
+                        rev = date_code[-1]
+                        date_code = date_code[:-1]
+
+                        readable_date = convert_date_code(date_code)
+
+                        embed.add_field(name=f":gear: Manufacturing Date", value=f'**```{readable_date}```**', inline=True)
+                        embed.add_field(name=f":hammer: Revision", value=f'**```{rev}```**', inline=True)
+                        await ctx.respond(embed=embed)
+                except Exception as e:
+                    await ctx.respond('<:warn:1105998033335898162> An error occurred when processing your command. Please contact `Angry_Pineapple#6926` with what you were attempting to do, along with the date and time.', ephemeral=True)
+                    print(f'[{requestTime}] [*** BOT ERROR ***] {e}')
+                    pass
+            
 
 def setup(bot):
-    bot.add_cog(SimplexDCD(bot))
+    bot.add_cog(DateCode(bot))
